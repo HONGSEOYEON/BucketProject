26903ed2a23b12b2d6be6bf9ec81d4f38433495e
jaewon <kosta@DESKTOP-3V2Q5FS>
2016-05-31 14:31:02 +0900

    BucketServiceLogic create

---
+++ Bucket_Project/src/main/java/com/kosta/bucket/service/BucketServiceLogic.java
@@ -0,0 +1,117 @@
+package com.kosta.bucket.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kosta.bucket.dao.BucketDao;
+import com.kosta.bucket.entity.Bucket;
+import com.kosta.bucket.entity.Comment;
+
+@Service
+public class BucketServiceLogic implements BucketService {
+
+	@Autowired
+	private BucketDao bucketDao;
+	
+	@Override
+	public int modifyBucket(Bucket bucket) {
+		return bucketDao.updateBucket(bucket);
+	}
+
+	@Override
+	public int removeBucket(String bucketId) {
+		return bucketDao.deleteBucket(bucketId);
+	}
+
+	@Override
+	public int registBucket(Bucket bucket) {
+		return bucketDao.createBucket(bucket);
+	}
+
+	@Override
+	public List<Bucket> searchBucketByContents(String contents) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bucket> searchBucketByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bucket> searchAllBucket() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int searchRecomNum(String bucketId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Bucket> searchBucketBestRecom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bucket searchBucket(String bucketId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int registRecommand(String bucketId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int registAccuse(String bucketId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int registComment(Comment comment) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeComment(String commentId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Comment> searchBucketComment(String bucketId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int removeAccusedBucket(String bucketId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Bucket> searchAccusedAllBucket() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bucket> searchMyBucket(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
