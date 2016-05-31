0c9ecc7fc8682a53405e95e891867bba39386750
seoyeon <kosta@DESKTOP-3V2Q5FS>
2016-05-31 14:56:38 +0900

    댓글 관리

--- Bucket_Project/src/main/java/com/kosta/bucket/dao/impl/BucketDaoImpl.java
+++ Bucket_Project/src/main/java/com/kosta/bucket/dao/impl/BucketDaoImpl.java
@@ -109,20 +109,47 @@ public class BucketDaoImpl implements BucketDao {
 
 	@Override
 	public int createComment(Comment comment) {
-		// TODO Auto-generated method stub
-		return 0;
+		SqlSession session = factory.openSession();
+		int result = 0;
+		try {
+			result = session.insert("insertComment", comment);
+			if(result > 0) {
+				session.commit();
+			} else {
+				session.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		return result;
 	}
 
 	@Override
 	public int deleteComment(String commentId) {
-		// TODO Auto-generated method stub
-		return 0;
+		SqlSession session = factory.openSession();
+		int result = 0;
+		try {
+			result = session.insert("deleteComment", commentId);
+			if(result > 0) {
+				session.commit();
+			} else {
+				session.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		return result;
 	}
 	
 	@Override
 	public List<Comment> retrieveBucketComment(String bucketId) {
-		// TODO Auto-generated method stub
-		return null;
+		SqlSession session = factory.openSession();
+		try {
+			List<Comment> comments = session.selectList("selectBucketComment", bucketId);
+			return comments;
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
