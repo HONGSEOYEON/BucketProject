a049e9f7211587060948e4d44ec6b71edd4c96ee
Seokdong <kosta@DESKTOP-3V2Q5FS>
2016-05-31 14:07:55 +0900

    d

--- Bucket_Project/src/main/java/com/kosta/bucket/controller/BucketController.java
+++ Bucket_Project/src/main/java/com/kosta/bucket/controller/BucketController.java
@@ -1,12 +1,12 @@
 package com.kosta.bucket.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kosta.bucket.entity.Bucket;
+import com.kosta.bucket.entity.Comment;
 import com.kosta.bucket.service.BucketService;
 
 @Controller
@@ -16,23 +16,49 @@ public class BucketController {
 	@Autowired
 	private BucketService bucketService;
 	
-	public ModelAndView modifyBucket(Bucket)
-	public ModelAndView removeBucket(bucketId) 
-	public ModelAndView registBucket(Bucket) 
-	public ModelAndView searchBucket(bucketId) 
-	public ModelAndView searchAccusedAllBucket()
+	public ModelAndView modifyBucket(Bucket bucket){
+		return null;
+	}
+	public ModelAndView removeBucket(String bucketId) {
+		return null;
+	}
+	public ModelAndView registBucket(Bucket bucket) {
+		return null;
+	}
+	public ModelAndView searchBucket(String bucketId) {
+		return null;
+	}
+	public ModelAndView searchAccusedAllBucket(){
+		return null;
+	}
 
-	public ModelAndView registRecommand(bucketId) 
-	public ModelAndView registAccuse(bucketId) 
+	public ModelAndView registRecommand(String bucketId) {
+		return null;
+	}
+	public ModelAndView registAccuse(String bucketId) {
+		return null;
+	}
 	 
-	public ModelAndView registComment (Comment) 
-	public ModelAndView removeComment (commentId) 
+	public ModelAndView registComment (Comment comment) {
+		return null;
+	}
+	public ModelAndView removeComment (String commentId) {
+		return null;
+	}
 	 
-	public ModelAndView removeAccusedBucket (bucketId)
+	public ModelAndView removeAccusedBucket (String bucketId){
+		return null;
+	}
 	 
-	public ModelAndView showModifyBucket(bucketId)
-	public ModelAndView showDetailBucket(bucketId) 
-	public ModelAndView showMyBucketList(userId)
+	public ModelAndView showModifyBucket(String bucketId){
+		return null;
+	}
+	public ModelAndView showDetailBucket(String bucketId) {
+		return null;
+	}
+	public ModelAndView showMyBucketList(String userId){
+		return null;
+	}
 	
 	
 }
--- Bucket_Project/src/main/java/com/kosta/bucket/controller/UserController.java
+++ Bucket_Project/src/main/java/com/kosta/bucket/controller/UserController.java
@@ -2,8 +2,8 @@ package com.kosta.bucket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kosta.bucket.entity.User;
 import com.kosta.bucket.service.UserService;
@@ -15,12 +15,28 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	public ModelAndView loginUser(User)  
-	public ModelAndView logoutUser(User) 
-	public ModelAndView joinUser(User) 
-	public ModelAndView removeUser(userId) 
-	public ModelAndView modifyUser(User) 
-	public ModelAndView showModifyPage()
-	public ModelAndView showLoginPage() 
-	public ModelAndView showJoinPage() 
+	public ModelAndView loginUser(User user){
+		return null;
+	}
+	public ModelAndView logoutUser(User user) {
+		return null;
+	}
+	public ModelAndView joinUser(User user) {
+		return null;
+	}
+	public ModelAndView removeUser(String userId) {
+		return null;
+	}
+	public ModelAndView modifyUser(User user) {
+		return null;
+	}
+	public ModelAndView showModifyPage(){
+		return null;
+	}
+	public ModelAndView showLoginPage() {
+		return null;
+	}
+	public ModelAndView showJoinPage() {
+		return null;
+	}
 }
