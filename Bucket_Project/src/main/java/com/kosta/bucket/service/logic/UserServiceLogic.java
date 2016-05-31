d76da937ef345e614f21039c69120bf51f952a44
sangmin <kosta@DESKTOP-3V2Q5FS>
2016-05-31 15:39:54 +0900

    user dao mapper user dao impl

---
+++ Bucket_Project/src/main/java/com/kosta/bucket/service/logic/UserServiceLogic.java
@@ -0,0 +1,39 @@
+package com.kosta.bucket.service.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kosta.bucket.dao.KeepBucketDao;
+import com.kosta.bucket.dao.UserDao;
+import com.kosta.bucket.entity.User;
+import com.kosta.bucket.service.BucketService;
+import com.kosta.bucket.service.UserService;
+
+public class UserServiceLogic implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Override
+	public int registUser(User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public User searchUser(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int modifyUser(User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeUser(String userId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
