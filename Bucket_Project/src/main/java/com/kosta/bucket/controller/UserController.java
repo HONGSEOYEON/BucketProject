0262ed47e4584b06e9a62ff0c05f9385c615caad
sangmin <kosta@DESKTOP-3V2Q5FS>
2016-05-31 15:05:26 +0900

    상민이 파일 오류 수정

--- Bucket_Project/src/main/java/com/kosta/bucket/controller/UserController.java
+++ Bucket_Project/src/main/java/com/kosta/bucket/controller/UserController.java
@@ -15,14 +15,9 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-<<<<<<< HEAD
 	@RequestMapping("/login")
-	public ModelAndView loginUser(User user) {
-
-=======
 	public ModelAndView loginUser(User user){
 		return null;
->>>>>>> branch 'master' of https://github.com/Cruel0412/BucketProject.git
 	}
 	
 	@RequestMapping("/logout")
@@ -44,15 +39,10 @@ public class UserController {
 	public ModelAndView modifyUser(User user) {
 		return null;
 	}
-<<<<<<< HEAD
 	
 	@RequestMapping("/")
-	public ModelAndView showModifyPage() {
-		
-=======
 	public ModelAndView showModifyPage(){
 		return null;
->>>>>>> branch 'master' of https://github.com/Cruel0412/BucketProject.git
 	}
 	public ModelAndView showLoginPage() {
 		return null;
