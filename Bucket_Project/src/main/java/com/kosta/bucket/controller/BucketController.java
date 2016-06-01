1a9990848457786b8592561b9b06f1113dd037c2
seoyeon <kosta@DESKTOP-3V2Q5FS>
2016-06-01 17:09:18 +0900

    상세페이지 연결

--- Bucket_Project/src/main/java/com/kosta/bucket/controller/BucketController.java
+++ Bucket_Project/src/main/java/com/kosta/bucket/controller/BucketController.java
@@ -226,17 +226,17 @@ public class BucketController {
 	
 	// 상세 페이지 
 	@RequestMapping(value="/detailBucket")
-	public ModelAndView showDetailBucket(/*String bucketId, HttpServletRequest req*/) {
+	public ModelAndView showDetailBucket(String bucketId/*, HttpServletRequest req*/) {
 		// 세션 아이디 가져오기
 		/*HttpSession session = req.getSession();
 		User user = (User) session.getAttribute("loginedUser");*/
 		
 		// 댓글 조회
-		List<Comment> comments= bucketService.searchBucketComment("1");
+		List<Comment> comments= bucketService.searchBucketComment(bucketId);
 		ModelAndView modelAndView = new ModelAndView("bucket/detailBucket");
 //		modelAndView.addObject("loginedUser", user.getUserId());
 		
-		Bucket bucket = bucketService.searchBucket("1");
+		Bucket bucket = bucketService.searchBucket(bucketId);
 		modelAndView.addObject("comments", comments);
 		modelAndView.addObject("bucket", bucket);
 		
